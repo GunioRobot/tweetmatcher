741e22ca65d0e50671a22c28b6b9f07fdd3ebecd
Jay Pipes <jaypipes@gmail.com>
2011-04-02 17:26:43 -0400

    Forgot to commit the tm/models.py... damn you git commit -a...

--- tm/models.py
+++ tm/models.py
@@ -1,28 +1,29 @@
 from django.db import models
 
+
 class Author(models.Model):
-    twitter_username = models.CharField(max_length=30)
-    email = models.EmailField()
+    username = models.CharField(max_length=30, unique=True)
+    screen_name = models.CharField(max_length=100)
+    profile_image_url = models.URLField()
     def __unicode__(self):
         return self.twitter_username
 
-class Tag(models.Model):
-    body = models.CharField(max_length=100)
-    authors = models.ManyToManyField(Author)
+
+class Tweet(models.Model):
+    twitter_id = models.BigIntegerField(unique=True)
+    body = models.CharField(max_length=140)
+    author = models.ForeignKey(Author)
+    created_at = models.DateTimeField()
     def __unicode__(self):
         return self.body
 
-class Tweet(models.Model):
-    body = models.CharField(max_length=140)
-    author = models.ForeignKey(Author)
-    def __unicode__(self):
-        return self.body
         
 class Hashtag(models.Model):
     text = models.CharField(max_length=139)
     author = models.ForeignKey(Author)
     def __unicode__(self):
         return self.text
+
         
 class Mention(models.Model):
     name = models.CharField(max_length=139)
